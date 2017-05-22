7fce09aa00d096e2b4f171e4e42a794e6133aa15
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-05-22 18:50:36 -0300

    removed login test code

--- index.js
+++ index.js
@@ -94,19 +94,4 @@ class Login {
     }
     return cont;
   }
-}
-
-const registeredUsers = {
-  user1: 'pass1',
-  user2: 'pass2',
-  user3: 'pass3',
 };
-
-const login = new Login(registeredUsers);
-
-login.registerUser('user4', 'pass4');
-login.login('user4', 'pass4');
-login.updatePassword('user3', 'pass3', 'pass5');
-login.login('user3', 'pass5');
-login.logout('user4');
-login.logout('user3');
