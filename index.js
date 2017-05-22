1d9ecacc3f4aa7eee1f0cf8ba6a0f06731b93bf4
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-05-22 16:14:27 -0300

    lint

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-'use strict';
-
 // This class is used for logins
+
 class Login {
   constructor(hash) {
     this.sessions = [];
     this.users = [];
     this.passwords = [];
-    Object.keys(hash).map(k => ({k, v: hash[k]})).map(e => {
+    Object.keys(hash).map(k => ({ k, v: hash[k] })).map((e) => {
       this.users = this.users.concat([e.k]);
       this.passwords = this.passwords.concat([e.v]);
     });
@@ -25,24 +24,24 @@ class Login {
   userExists(user) {
     // Temp variable for storing the user if found
     let temp = '';
-    for (let i of this.users) {
+    for (const i of this.users) {
       if (i === user) {
         temp = user;
       }
     }
-    let exists = (temp !== '' && temp === user);
+    const exists = (temp !== '' && temp === user);
     return exists;
   }
 
   // Register user
   registerUser(user, password) {
-    let lastIndex = this.users.length;
+    const lastIndex = this.users.length;
     this.users[lastIndex] = user;
     this.passwords[lastIndex] = password;
   }
 
   removeUser(user) {
-    let index = this.idx(user, this.users);
+    const index = this.idx(user, this.users);
     this.users[index] = null;
     this.passwords[index] = null;
     this.users = this.users.filter(user => user !== null);
@@ -50,21 +49,21 @@ class Login {
   }
 
   checkPassword(user, password) {
-    let index = this.idx(user, this.users);
-    let passwordCorrect = this.passwords[index] === password;
+    const index = this.idx(user, this.users);
+    const passwordCorrect = this.passwords[index] === password;
     return passwordCorrect;
   }
 
   updatePassword(user, oldPassword, newPassword) {
     // First we check if the user exists
     let user1 = '';
-    for (let i of this.users) {
+    for (const i of this.users) {
       if (i === user) {
         user1 = user;
       }
     }
     if (user1 === user) {
-      let index = this.idx(user, this.users);
+      const index = this.idx(user, this.users);
       if (this.passwords[index] === oldPassword) {
         this.passwords[index] = newPassword;
         return true;
@@ -74,7 +73,7 @@ class Login {
   }
 
   login(user, password) {
-    let index = this.idx(user, this.users);
+    const index = this.idx(user, this.users);
     if (this.passwords[index] === password) {
       this.sessions.push(user);
     }
@@ -82,8 +81,8 @@ class Login {
 
   // Gets index of an element in an array
   idx(element, array) {
-    let cont=0;
-    for (let i of array) {
+    let cont = 0;
+    for (const i of array) {
       if (i === element) {
         return cont;
       }
@@ -93,13 +92,13 @@ class Login {
   }
 }
 
-let registeredUsers = {
+const registeredUsers = {
   user1: 'pass1',
   user2: 'pass2',
-  user3: 'pass3'
+  user3: 'pass3',
 };
 
-let login = new Login(registeredUsers);
+const login = new Login(registeredUsers);
 
 login.registerUser('user4', 'pass4');
 login.login('user4', 'pass4');
